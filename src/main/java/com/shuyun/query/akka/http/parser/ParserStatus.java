0ad53a07b738e21bc48b85cb4510217053dce298
huangxiuyuan <xiuyuan.huang@shuyun.com>
2017-03-21 15:31:30 +0800

    rm -commit

--- src/main/java/com/shuyun/query/akka/http/parser/ParserStatus.java
+++
@@ -1,8 +0,0 @@
-package com.shuyun.query.akka.http.parser;
-
-public enum ParserStatus {
-	START,
-	ARRAY_START,
-	OBJECT_START,
-	OBJECT_END
-}
--- src/main/java/com/shuyun/query/meta/FileType.java
+++
@@ -1,8 +0,0 @@
-package com.shuyun.query.meta;
-
-/**
- * Created by wanghaiwei on 2016/1/20.
- */
-public enum FileType {
-    collect,order,weblog_followup,weblog_pc;
-}
